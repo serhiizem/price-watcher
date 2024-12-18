c8eec1f21abe9019372cdf8345094f0b4f55acd8
serhiizem <serhiizem@gmail.com>
2024-12-18 15:45:27 +0200

    [PW-1] correctly resolve .env location

--- deployment/lib/config/app-config.ts
+++ deployment/lib/config/app-config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
+import * as path from "node:path";
 
-dotenv.config();
+dotenv.config({path: path.resolve(__dirname, "../../../.env")});
 
 export const AppConfig = {
     cdkUsername: process.env.CDK_USERNAME as string
